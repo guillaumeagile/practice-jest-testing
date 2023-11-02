1f4ef9daafb82a59adda2a4c412bf4d93e519344
guillaume <guillaume.saintetienne@marvin-bot.com>
2023-11-02 17:09:04 +0100

    composition à terminer

--- _js/tests/exercice1.test.js
+++ _js/tests/exercice1.test.js
@@ -1,6 +1,18 @@
 
+const addOne = (a) => a + 1
+const multTwo = (a) => a * 2
+const addOneMultTwo = (a) => multTwo(addOne(a))
 
+
+// returns 12
 
 test('should reverse normal strings', () => {
-    expect(true).toBe(true);
-});
+    expect(addOneMultTwo(5) ).toBe(12)
+})
+
+
+const addOneMultTwoComposed = compose(multTwo, addOne)
+
+test('should reverse normal strings', () => {
+    expect(addOneMultTwoComposed(5) ).toBe(12)
+})
