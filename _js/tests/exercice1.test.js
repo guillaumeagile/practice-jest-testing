31702bf7662aa34e7918061131d7a6bd62d621a4
Fabio <fabiobentaila@gmail.com>
2023-11-02 17:25:56 +0100

    test working with compose

--- _js/tests/exercice1.test.js
+++ _js/tests/exercice1.test.js
@@ -4,15 +4,17 @@ const multTwo = (a) => a * 2
 const addOneMultTwo = (a) => multTwo(addOne(a))
 
 
+
+
 // returns 12
 
 test('should reverse normal strings', () => {
     expect(addOneMultTwo(5) ).toBe(12)
 })
 
-
+const compose = (f,g) => (a) => f(g(a))
 const addOneMultTwoComposed = compose(multTwo, addOne)
 
-test('should reverse normal strings', () => {
-    expect(addOneMultTwoComposed(5) ).toBe(12)
+test('compose of two functions', () => {
+    expect(addOneMultTwoComposed(5)).toBe(12)
 })
